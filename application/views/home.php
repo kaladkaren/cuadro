68a83d318dfe1484c28b35066286a44fb3feb58b
dev\ryan <username.magsakay@gmail.com>
2021-04-22 15:57:49 +0800

    Update home.php

--- application/views/home.php
+++ application/views/home.php
@@ -5,8 +5,27 @@
 </head>
 <body>
 
-</body>
+<!-- <input type="filepicker" data-fp-apikey="AfLddltxlRFyI7PzHfgR4z" /> -->
 
-<input type="filepicker" data-fp-apikey="AfLddltxlRFyI7PzHfgR4z" />
+<button id="facebookUpload">Upload your Photos for Facebook</button>
 <script type="text/javascript" src="https://api.filestackapi.com/filestack.js"></script>
+
+<script>
+	filepicker.setKey("AfLddltxlRFyI7PzHfgR4z");
+	
+	document.getElementById('facebookUpload').onclick = function(){
+
+		filepicker.pick(
+		{
+			mimetype: 'image/*',
+			services: ['COMPUTER','FACEBOOK','INSTAGRAM']
+		},
+		function(Blob){
+			console.log(JSON.stringify(Blob));
+		}
+		);
+	};
+</script>
+
+</body>
 </html>
