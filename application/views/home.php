7b1ee8515a985c20af7ee95b13d50ef48ea1ac16
dev\ryan <username.magsakay@gmail.com>
2021-04-22 15:33:56 +0800

    Update home.php

--- application/views/home.php
+++ application/views/home.php
@@ -7,6 +7,6 @@
 
 </body>
 
-<input type="filepicker" data-fp-apikey="AfLddltxlRFyI7PzHfgR4z">
+<input type="filepicker" data-fp-apikey="AfLddltxlRFyI7PzHfgR4z" />
 <script src="//static.filestackapi.com/filestack-js/3.x.x/filestack.min.js"></script>
 </html>
