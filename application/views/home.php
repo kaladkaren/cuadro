9c43d7773ec98a3f687320f8cffa9944c2e4bdc0
dev\ryan <username.magsakay@gmail.com>
2021-04-22 14:49:17 +0800

    Update home.php

---
+++ application/views/home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cuadro</title>
+</head>
+<body>
+
+</body>
+
+
+<script type="text/javascript">
+	const client = filestack.init(AfLddltxlRFyI7PzHfgR4z);
+	client.picker().open();
+</script>
+<script src="//static.filestackapi.com/filestack-js/3.x.x/filestack.min.js"></script>
+</html>
