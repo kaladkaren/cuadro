c8ca617cd422d6b1b6253c3470323c7ddb1fb922
dev\ryan <username.magsakay@gmail.com>
2021-04-23 13:37:19 +0800

    form fields multiple data

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -8,11 +8,15 @@ class Home extends Admin_core_controller {
     parent::__construct();
 
     $this->load->model('cms/Order_model');
+    $this->load->model('cms/Frame_model');
   }
 
   public function index()
   {
-    $this->load->view('frontend/home');
+    $res = $this->Frame_model->all();
+
+    $data['res'] = $res;
+    $this->load->view('frontend/home', $data);
   }
 
   public function add()
--- application/views/Frontend/home.php
+++ application/views/Frontend/home.php
@@ -9,8 +9,22 @@
 <input type="text" name="customer_name" placeholder="Enter your Name"><br>
 <input type="email" name="customer_email" placeholder="Enter mailing address"><br>
 <input type="text" name="customer_contactnum" placeholder="Enter Contact Number"><br>
+<select name="order_type">
+
+	<?php if (count($res) > 0 ): ?>
+
+        <?php $i = 1; foreach ($res as $key => $value): ?>
+            <option><?php echo $value->frame_type;?></option>
+        <?php endforeach; ?>
+
+
+    <?php else: ?>
+        <p>empty</p>
+    <?php endif; ?>
+	
+</select><br>
 <textarea name="customer_address" placeholder="Address"></textarea><br>
-<input type="file" name="order_images"><br>
+<input type="file" name="order_images[]" multiple accept="image/*"><br>
 
 </form>
 
